92d39b82f7fc32d4f28a5d11e90f41cdf148697f
Generally-general <kumarpranjal4@gmail.com>
2026-01-16 10:49:20 +0530

    Refactor: add PingService and Constructor injection

--- src/main/java/com/myProject/demo/PingController.java
+++ src/main/java/com/myProject/demo/PingController.java
@@ -1,12 +1,19 @@
 package com.myProject.demo;
 
+import com.myProject.demo.service.PingService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingController {
+    private final PingService pingService;
+
+    public PingController(PingService pingService) {
+        this.pingService = pingService;
+    }
+
     @GetMapping("/ping")
-    public String sayPing() {
-        return "ok";
+    public String ping() {
+        return pingService.ping();
     }
 }
