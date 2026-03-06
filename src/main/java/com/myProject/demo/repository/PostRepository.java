c5253ea3f4652fd96fdbfb71f5856503ab605eed
Generally-general <kumarpranjal4@gmail.com>
2026-03-06 21:46:38 +0530

    Fix N+1 query problem in posts retrieval using EntityGraph to fetch users efficiently

--- src/main/java/com/myProject/demo/repository/PostRepository.java
+++ src/main/java/com/myProject/demo/repository/PostRepository.java
@@ -4,9 +4,13 @@ import com.myProject.demo.entity.Post;
 import com.myProject.demo.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    @EntityGraph(attributePaths = "user")
+    Page<Post> findAll(Pageable pageable);
+
     Page<Post> findByUser(
             User user,
             Pageable pageable
